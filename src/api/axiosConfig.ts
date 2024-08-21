1ffa9d37e6db090cd28c32ab03d2633a6b373a9a
haagen24 <40901301+haagen24@users.noreply.github.com>
2024-08-21 17:58:23 -0400

    change to use env baseurl

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
+import "dotenv/config";
 
 export default axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:8080/",
     headers: { "ngrok-skip-browser-warning": "true" },
 });
